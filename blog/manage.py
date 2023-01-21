034b94dc9eb501fd51f417835522f40f50af2856
KartikKanugo <karthik.kanugo@gmail.com>
2023-01-21 12:46:27 +0100

    linux setting

--- blog/manage.py
+++ blog/manage.py
@@ -5,7 +5,7 @@ Command-line utility for administrative tasks.
 # For more information about this file, visit
 # https://docs.djangoproject.com/en/2.1/ref/django-admin/
 """
-
+# Opened in linux
 import os
 import sys
 
